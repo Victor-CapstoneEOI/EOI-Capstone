9cf29cf6decc6147a5432c1fe8e63e8f0e53885b
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-08-28 15:09:51 -0400

    start schema for parent questions

--- src/Parent.jsx
+++ src/Parent.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 
 export const Parent = () => {
+    
+    // to populate schema 
+    let type = ''
+    let title = '' // this is the key in the object 
+
+    // to populate UI schema 
+    let label = ''
+    let typeUI = ''
+
+    let question = {
+        "properties" : {
+            title: {
+                "type" : type
+            }
+        }
+    }
+
+    let uiSchema = {
+        "type": typeUI,
+        "scope": title, 
+        "label": label
+    }
+
+
   return (
     <div>Parent</div>
   )

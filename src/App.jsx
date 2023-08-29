dc13dd84182d73c980ff33ee6457bfacc82fe080
Melissa Schaefer <mschaefer@xpanxion.com>
2023-08-29 14:57:47 -0400

    update with tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Home";
 import StepperComponent from "./Stepper";
 import "./App.css";
+// import PDF from "./PDF" 
 
 function App() {
   return (
     <div className="App">
         <Home />
         <StepperComponent />
+         {/* <PDF/> */}
     </div>
   );
 }

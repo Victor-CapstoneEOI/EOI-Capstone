dd03be4697eb6239da4546b9de3961fea942a15b
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-08-29 16:41:30 -0400

    fix package.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Home";
 // import PDF from "./PDF" 
 import "./App.css";
+import { Parent } from "./Parent";
 
 function App() {
   return (
     <div className="App">
         <Home />
         {/* <PDF/> */}
+        <Parent />
     </div>
   );
 }

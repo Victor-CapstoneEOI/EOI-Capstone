9cc676ab4eed3b69d6124e66a4fac8a477056f25
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-08-29 11:53:09 -0400

    Config server file (#5)

--- server/server.js
+++ server/server.js
@@ -4,21 +4,23 @@ import apiRoutes from './routes/api.js'
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/your-database', {
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://lmercedes03:kWfRIlNOecaTp8rU@cluster0.wssqiab.mongodb.net/eoi_form?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB:', err.message));
 
-
+// Set up routes
 app.get("/", (req, res) => {
   res.send("Welcome to the homepage!");
 });
 
-
 app.use('/api', apiRoutes);
 
+// Start the server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

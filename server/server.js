04d8b74b5a307e68fac0dba9bd027b716319d232
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-08-29 11:10:53 -0400

    Config routes directory

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import apiRoutes from './routes/api.js'
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/your-database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the homepage!");
 });
 
 
+app.use('/api', apiRoutes);
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+

93f9d8f31c0beb24fffefc5b009e6755527006ee
Melissa Schaefer <mschaefer@xpanxion.com>
2023-08-29 17:50:59 -0400

    update with progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Home from "./Home";
-import StepperComponent from "./Stepper";
+// import Stepper from './Stepper'; 
 import "./App.css";
 // import PDF from "./PDF" 
 
 function App() {
   return (
+    
     <div className="App">
+
+{/* <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-semibold mb-4">Horizontal Stepper Example</h1>
+      <Stepper />
+    </div> */}
+
         <Home />
-        <StepperComponent />
+       
          {/* <PDF/> */}
     </div>
   );
--- src/Stepper.jsx
+++ src/Stepper.jsx
@@ -1,97 +1,70 @@
 import React, { useState } from 'react';
-import {
-  Stepper,
-  Step,
-  Button,
-  Progress,
-  Div,
-  Flex,
-  Text,
-} from 'tailwindcss-react';
 
-const StepperComponent = () => {
-  const [currentStep, setCurrentStep] = useState(1);
+const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4', 'Step 5', 'Step 6'];
 
-  const steps = [
-    {
-      title: 'Step 1',
-      content: 'This is the first step.',
-    },
-    {
-      title: 'Step 2',
-      content: 'This is the second step.',
-    },
-    {
-      title: 'Step 3',
-      content: 'This is the third step.',
-    },
-    {
-      title: 'Step 4',
-      content: 'This is the fourth step.',
-    },
-    {
-      title: 'Step 5',
-      content: 'This is the fifth step.',
-    },
-    {
-      title: 'Step 6',
-      content: 'This is the sixth step.',
-    },
-  ];
+const Stepper = () => {
+  const [activeStep, setActiveStep] = useState(0);
 
-  const handleBackClick = () => {
-    setCurrentStep(currentStep - 1);
+  const handleNext = () => {
+    setActiveStep(prevStep => Math.min(prevStep + 1, steps.length - 1));
   };
 
-  const handleNextClick = () => {
-    setCurrentStep(currentStep + 1);
+  const handleBack = () => {
+    setActiveStep(prevStep => Math.max(prevStep - 1, 0));
   };
 
-  const handleResetClick = () => {
-    setCurrentStep(1);
+  const handleReset = () => {
+    setActiveStep(0);
   };
+
+  const isLastStep = activeStep === steps.length - 1;
 
   return (
-    <Div>
-      <Stepper>
-        {steps.map((step, index) => (
-          <Step
-            key={index}
-            active={currentStep === index + 1}
-            title={step.title}
-            content={step.content}
-          ></Step>
-        ))}
-      </Stepper>
-      <Flex justifyContent="center">
-        <Button
-          variant="primary"
-          disabled={currentStep === 1}
-          onClick={handleBackClick}
-        >
-          Back
-        </Button>
-        <Button
-          variant="primary"
-          disabled={currentStep === steps.length}
-          onClick={handleNextClick}
-        >
-          Next
-        </Button>
-        <Button
-          variant="primary"
-          onClick={handleResetClick}
-        >
-          Reset
-        </Button>
-      </Flex>
-      {currentStep === steps.length && (
-        <Text>
-          All steps complete!
-        </Text>
-      )}
-    </Div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+        <div className="flex items-center justify-between mb-4">
+          <button
+            onClick={handleBack}
+            disabled={activeStep === 0}
+            className={`text-blue-500 hover:text-blue-700 ${activeStep === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+          >
+            Back
+          </button>
+          <div className="flex items-center space-x-4 flex-grow">
+            <div className="relative h-2 flex-grow">
+              <div
+                className="absolute h-2 bg-blue-500"
+                style={{
+                  width: `${(activeStep / (steps.length - 1)) * 100}%`,
+                }}
+              />
+            </div>
+            <div className="text-lg font-semibold">{steps[activeStep]}</div>
+          </div>
+          <div className="space-x-2">
+            <button
+              onClick={handleReset}
+              className="text-red-500 hover:text-red-700"
+            >
+              Reset
+            </button>
+            <button
+              onClick={handleNext}
+              disabled={isLastStep}
+              className={`text-blue-500 ${isLastStep ? 'opacity-50 cursor-not-allowed' : 'hover:text-blue-700'}`}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-between mt-4">
+          {isLastStep && (
+            <div className="text-green-600 font-semibold">All Steps Complete!</div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default StepperComponent;
+export default Stepper;

1d7f8c70a4156f628aebb353121671425b68defe
Karina Velasquez <kvelasquez@xpanxion.com>
2023-08-29 17:49:40 -0400

    Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Home from "./Home";
+import Profile from './Profile'
+import FormLayout from './FormLayout'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import PDF from "./PDF" 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <Home />
+      <BrowserRouter>
+      <Routes>
+
+        <Route path = '/' exact={true} element={<Home />}/>
+        <Route path='/profile' exact ={true} element={<Profile/>}/>
+        <Route path = '/formlayout' exact={true} element={<FormLayout/>}/>
         {/* <PDF/> */}
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/FormLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+
+
+
+export const FormLayout = () => {
+  return (
+    <h1>
+        
+        FormLayout
+    
+    
+    
+    
+    
+    
+    </h1>
+  )
+}
+
+export default FormLayout;

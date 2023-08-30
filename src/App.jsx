6d3fb881d7bdba8f4dbe42c3baa9bac6efe45bb1
Melissa Schaefer <mschaefer@xpanxion.com>
2023-08-30 05:10:40 -0400

    update with component edits

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import React from "react";
 // import PDF from "./PDF" 
 // import "./App.css";
 import FormComponent from "./FormComponent";
+import  Stepper from './Stepper';
+import './Stepper.css'; // 
+
+
+const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
+
 
 function App() {
   return (
     <div className="App">
         {/* <Home /> */}
         {/* <PDF/> */}
+        <Stepper steps={steps} />
         <FormComponent/>
+       
+   
         
     </div>
   );

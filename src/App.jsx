6092f3fdfb14bff469d14d992f5fc05564704ef2
mschaefer <lebruinm@gmail.com>
2023-08-30 15:19:10 -0400

    frontend update with footer component (#19)
    * update with component edits
    
    * upate with labels+progressbar+upatedcss
    
    * update with frontend styling
    
    * update frontend
    
    ---------
    
    Co-authored-by: Melissa Schaefer <mschaefer@xpanxion.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import Home from "./Home";
 import Profile from './Profile'
 import FormLayout from './FormLayout'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import PDF from "./PDF" 
+// import PDF from "./PDF
+import Footer from "./Footer";
 // import "./App.css";
 // import { Parent } from "./Parent";
 // import FormComponent from "./FormComponent";
 // import  Stepper from './Stepper';
 // import './Stepper.css'; // 
 
-
-// const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
+const steps = ['Profile', 'Lifestyle', 'Wellness', 'Medical Information', 'Authorization and Consent'];
 
 
 function App() {
@@ -24,10 +24,12 @@ function App() {
         <Route path='/profile' exact ={true} element={<Profile/>}/>
         <Route path = '/formlayout' exact={true} element={<FormLayout/>}/>
         {/* <PDF/> */}
+        <Stepper steps={steps} />
         </Routes>
         </BrowserRouter>
         {/* <Stepper steps={steps} /> */}
         {/* <FormComponent/> */}
+        <Footer />
        
    
         

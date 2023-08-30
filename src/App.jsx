82465ffeec943440c10f06d744364b8c593295bd
mschaefer <lebruinm@gmail.com>
2023-08-30 07:27:58 -0400

    update with component edits (#16)
    Co-authored-by: Melissa Schaefer <mschaefer@xpanxion.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import React from "react";
 // import "./App.css";
 import { Parent } from "./Parent";
 import FormComponent from "./FormComponent";
+import  Stepper from './Stepper';
+import './Stepper.css'; // 
+
+
+const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
+
 
 function App() {
   return (
     <div className="App">
         {/* <Home /> */}
         {/* <PDF/> */}
+        <Stepper steps={steps} />
         <FormComponent/>
+       
+   
         
         <Parent index = {4}/>
     </div>

c7ee83d30c15a1a00e373ed5c8650fe61912abed
Karina Velasquez <kvelasquez@xpanxion.com>
2023-08-29 23:34:47 -0400

    FormLayout

--- src/FormLayout.jsx
+++ src/FormLayout.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
-
-
+import CornerLogo from './img/cornervictor.png';
+import './FromLayout.css'
 
 export const FormLayout = () => {
   return (
-    <h1>
-        
-        FormLayout
-    
-    
-    
-    
-    
-    
-    </h1>
-  )
+    <div>
+      <img src={CornerLogo} alt="cornervictor" className="cornerlogo" />
+      <h1 className='Complete'>
+        Complete <span className='Rest'>Your Evidence of Insurability Form</span>
+      </h1>
+      <div className='parent-container'>
+        <div className='medical-questions-title'>Medical Questions</div>
+        <div className='container'>
+          {/* Your medical questions content goes here */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FormLayout;

58316504fdeb27b382961ed6e2f20727cdda78e8
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-09-02 21:54:52 -0400

    Start wellness section #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 // App.js
 import React from "react";
-// import Home from "./Pages/Home.jsx";
-// import Profile from './Pages/Profile.jsx'
-// import FrenchProfile from "./Pages/FrenchProfile.jsx";
-// import FormLayout from './Pages/FormLayout.jsx'
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { LifeStyleSection } from "./sections/LifeStyleSection";
 import { Wellness } from "./sections/Wellness";
-import { PastApplications } from "./sections/PastApplications";
-
-
-
-
-
-import Header from './Components/Header';
-import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -32,7 +18,7 @@ function App() {
       <Footer /> */}
       {/* <LifeStyleSection index={0} /> */}
       {/* <Wellness/> */}
-      <PastApplications/>
+      <Wellness/>
     </div>
   );
 }
---
+++ src/sections/Well.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Well = () => {
+  return (
+    <div>Well</div>
+  )
+}

53d4bc8b032c883656f1d63d40d018497ded4769
imaneAllay <73351438+imaneAllay@users.noreply.github.com>
2023-09-03 23:15:28 -0400

    Update Validation for LF section and Adjust jsonForms Style (#45)
    * Adjust Json forms Style and LS section Field validation
    
    * Adjust LS section and JsonForms style
    
    ---------
    
    Co-authored-by: Imane Allay <iallay@xpanxion.com>

--- src/Pages/FormLayout.jsx
+++ src/Pages/FormLayout.jsx
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import { useNavigate } from 'react-router';
 import { PersonalInformation } from '../sections/personalInformation.jsx';
@@ -6,32 +8,40 @@ import { MedicalSection } from '../sections/MedicalSection.jsx';
 import Sidebar from '../Components/Sidebar.jsx';
 import '../Styles/FormLayout.css'
 
-export const FormLayout = () => {
 
+export const FormLayout = () => {
   const navigate = useNavigate();
 
-  const goToReview = () =>{
-    navigate('/review');  
-  }
 
-    return (
-      <div>
-        <h1 className='Complete'>
-          Complete <span className='Rest'>Your Evidence of Insurability Form</span>
-        </h1>
-        <div className='parent'>
-          <div className='medical-questions-title'>Medical Questions</div>
-            <div className='Inputbox'>
-                {/* <Stepper/> */}
-                <PersonalInformation/>
-                {/* <LifeStyleSection index={0} /> */}
-                {/* <MedicalSection/> */}
-            </div>
-          <button className='next-button' onClick={goToReview}>Next Page</button>
-          <Sidebar/>
+  const goToReview = () => {
+    navigate("/review");
+  };
+
+  return (
+    <div>
+      <h1 className="Complete">
+        Complete{" "}
+        <span className="Rest">Your Evidence of Insurability Form</span>
+      </h1>
+
+      <div className="parent">
+        <div className="medical-questions-title"> EOI Form </div>
+
+        <div className="jsonForms">
+          {/* <Stepper/> */}
+          <div className="section">
+            <PersonalInformation />
+            <LifeStyleSection index={0} />
+            <MedicalSection />
+          </div>
         </div>
+        <button className="next-button" onClick={goToReview}>
+          Next Page
+        </button>
+        <Sidebar />
       </div>
-    );
-  }
+    </div>
+  );
+};
 
-export default FormLayout;
+  

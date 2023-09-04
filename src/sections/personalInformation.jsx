eadb98c44514e08fa1dbb67d8b2cd41e6efb128f
Leonard <lemrcedes030@gmail.com>
2023-09-04 15:39:42 -0400

    Modified Personal Information section to utilize useContext

--- src/sections/personalInformation.jsx
+++ src/sections/personalInformation.jsx
@@ -1,15 +1,11 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { JsonForms } from "@jsonforms/react";
 import { materialRenderers } from "@jsonforms/material-renderers";
 import { getSchemaForQuestion, getUiSchemaForQuestion } from '../schemas/schemaUtils';
 import FormContext from "../Components/FormContext";
 
-
 export const PersonalInformation = () => {
-  const { updateFormData } = useContext(FormContext);
-
-  const [formData, setFormData] = useState({});
-  const formDataRef = useRef(formData);
+  const { formData, updateFormData, activeSection, setActiveSection } = useContext(FormContext);
 
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -24,23 +20,9 @@ export const PersonalInformation = () => {
       .then(data => {
         const filteredQuestions = data.filter(q => q.section === "Personal information");
         setQuestions(filteredQuestions);
-        setCurrentQuestionIndex(0); 
-        console.log(filteredQuestions)
       })
       .catch(error => console.error("Error fetching questions:", error));
   }, []);
-
-
-  useEffect(() => {
-    const savedData = sessionStorage.getItem("formData");
-    if (savedData) {
-      setFormData(JSON.parse(savedData));
-    }
-  }, []);
-
-  useEffect(() => {
-    formDataRef.current = formData; // Keep the ref updated with the latest value
-  }, [formData]);
 
   useEffect(() => {
     const currentQuestion = questions[currentQuestionIndex];
@@ -52,21 +34,14 @@ export const PersonalInformation = () => {
     const schema = getSchemaForQuestion(currentQuestion);
     const requiredFields = schema.required || [];
 
-    // If the question is optional, do not check for empty fields and allow users to proceed
     if (currentQuestion.questionText.toLowerCase().includes("(optional)")) {
       setIsFieldEmpty(false);
       return;
     }
 
-    const emptyFields = requiredFields.filter(field => !formData[field]);
+    const emptyFields = requiredFields.filter(field => !formData[currentQuestion.questionText]);
     setIsFieldEmpty(emptyFields.length > 0);
-}, [currentQuestionIndex, questions, formData]);
-
-
-  useEffect(() => {
-    const currentFormData = JSON.parse(sessionStorage.getItem(`formData-${currentQuestionIndex}`) || '{}');
-    setFormData(currentFormData);
-  }, [currentQuestionIndex]);
+  }, [currentQuestionIndex, questions, formData]);
 
   const handleKeyPress = event => {
     if (event.key === "Enter") {
@@ -81,16 +56,17 @@ export const PersonalInformation = () => {
     }
     const nextIndex = currentQuestionIndex + 1;
     if (nextIndex < questions.length) {
-        sessionStorage.setItem(`formData-${currentQuestionIndex}`, JSON.stringify(formData));
-        updateFormData({ [currentQuestion.questionText]: formData }); // Store the form data in the context
+        updateFormData({ [currentQuestion.questionText]: formData[currentQuestion.questionText] });
         setCurrentQuestionIndex(nextIndex);
+    } else {
+        // Assuming you would like to increment the activeSection to move to the next form
+        setActiveSection(activeSection + 1);
     }
   };
 
   const handlePrevious = () => {
     const prevIndex = currentQuestionIndex - 1;
     if (prevIndex >= 0) {
-        sessionStorage.setItem(`formData-${currentQuestionIndex}`, JSON.stringify(formData));
         setCurrentQuestionIndex(prevIndex);
     }
   };
@@ -101,14 +77,8 @@ export const PersonalInformation = () => {
     return <div>Form Complete</div>;
   }
 
-  console.log('Current Question:', currentQuestion);
-  console.log('Schema:', getSchemaForQuestion(currentQuestion));
-  console.log('UI Schema:', getUiSchemaForQuestion(currentQuestion));
-  console.log('Form Data:', formData);
- 
-
   return (
-    <div onKeyDown={handleKeyPress} >
+    <div onKeyDown={handleKeyPress}>
       <h2>{currentQuestion.section}</h2>
       {currentQuestion.subSection1 && <h3>{currentQuestion.subSection1}</h3>}
       {currentQuestion.subSection2 && <h4>{currentQuestion.subSection2}</h4>}
@@ -117,9 +87,9 @@ export const PersonalInformation = () => {
         key={currentQuestionIndex}
         schema={getSchemaForQuestion(currentQuestion)}
         uischema={getUiSchemaForQuestion(currentQuestion)}
-        data={formData}
+        data={formData[currentQuestion.questionText] || {}}
         renderers={materialRenderers}
-        onChange={({ data }) => setFormData(data)}
+        onChange={({ data }) => updateFormData({ [currentQuestion.questionText]: data })}
         liveValidate={true}
       />
 

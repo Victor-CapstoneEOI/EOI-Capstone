11f4ff65bdcc0196cf049266035eb56c7b07d3ae
Leonard <lemrcedes030@gmail.com>
2023-09-03 22:36:08 -0400

    Added FormContext component for context api, update schemaUtils

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FormProvider } from "./Components/FormContext.jsx";
 import Home from "./Pages/Home.jsx";
 import Profile from './Pages/Profile.jsx'
 import FrenchProfile from "./Pages/FrenchProfile.jsx";
 import FormLayout from './Pages/FormLayout.jsx';
 import Review from "./Pages/Review.jsx";
 import Confirmation from './Pages/Confirmation.jsx';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Benefits from './img/removebg.png'
-// import { FormComponent } from "./FormComponent";
-// import { LifeStyleSection } from "./sections/LifeStyleSection";
 import Header from './Components/Header';
 import Footer from "./Components/Footer";
+// import Benefits from './img/removebg.png'
 
 
 function App() {
@@ -18,6 +17,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
+        <FormProvider >
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
@@ -26,6 +26,7 @@ function App() {
           <Route path='/review'  element = {<Review/>}/>
           <Route path = "/confirmation" element={<Confirmation/>}/>
         </Routes>
+        </FormProvider>
       </BrowserRouter>
       {/* <img src={Benefits} alt='Benefits' className="group-benefits" /> */}
       <Footer />
--- src/Pages/FormLayout.jsx
+++ src/Pages/FormLayout.jsx
@@ -1,46 +1,37 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
-import PersonalInformation from '../sections/PersonalInformation.jsx';
+import { PersonalInformation } from '../sections/personalInformation.jsx';
+import { LifeStyleSection } from '../sections/LifeStyleSection.jsx';
+import { MedicalSection } from '../sections/MedicalSection.jsx';
 import Sidebar from '../Components/Sidebar.jsx';
 import '../Styles/FormLayout.css'
-import { LifeStyleSection } from '../sections/LifeStyleSection.jsx';
-import Header from '../Components/Header.jsx';
-// import { MedicalSection } from '../sections/MedicalSection.jsx';
-// import Stepper from '../Components/Stepper.jsx'
-
 
 export const FormLayout = () => {
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const goToReview = () =>{
-  navigate('/review');  
-}
+  const goToReview = () =>{
+    navigate('/review');  
+  }
 
-
-  return (
-    <div>
-     
-      <h1 className='Complete'>
-        Complete <span className='Rest'>Your Evidence of Insurability Form</span>
-      </h1>
-
- 
-
-      <div className='parent'>
-        <div className='medical-questions-title'>Medical Questions</div>
-        
-        <div className='Inputbox'>
-          {/* <Stepper/> */}
-          <PersonalInformation/>
-          <LifeStyleSection index={0} />
-          {/* <MedicalSection/> */}
+    return (
+      <div>
+        <h1 className='Complete'>
+          Complete <span className='Rest'>Your Evidence of Insurability Form</span>
+        </h1>
+        <div className='parent'>
+          <div className='medical-questions-title'>Medical Questions</div>
+            <div className='Inputbox'>
+                {/* <Stepper/> */}
+                <PersonalInformation/>
+                {/* <LifeStyleSection index={0} /> */}
+                {/* <MedicalSection/> */}
+            </div>
+          <button className='next-button' onClick={goToReview}>Next Page</button>
+          <Sidebar/>
         </div>
-        <button className='next-button' onClick={goToReview}>Next Page</button>
-        <Sidebar/>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 export default FormLayout;

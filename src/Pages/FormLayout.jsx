252e8bfb9fda0eb764629c8025b2e6d1833383c0
Imane Allay <iallay@xpanxion.com>
2023-09-03 23:09:34 -0400

    Adjust LS section and JsonForms style

--- src/Pages/FormLayout.jsx
+++ src/Pages/FormLayout.jsx
@@ -1,48 +1,45 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import PersonalInformation from '../sections/PersonalInformation.jsx';
-import Sidebar from '../Components/Sidebar.jsx';
-import '../Styles/FormLayout.css'
-import { LifeStyleSection } from '../sections/LifeStyleSection.jsx';
-import Header from '../Components/Header.jsx';
-import { MedicalSection } from '../sections/MedicalSection.jsx';
+import React from "react";
+import { useNavigate } from "react-router";
+import PersonalInformation from "../sections/PersonalInformation.jsx";
+import Sidebar from "../Components/Sidebar.jsx";
+import "../Styles/FormLayout.css";
+import { LifeStyleSection } from "../sections/LifeStyleSection.jsx";
+import Header from "../Components/Header.jsx";
+import { MedicalSection } from "../sections/MedicalSection.jsx";
 // import Stepper from '../Components/Stepper.jsx'
 
-
 export const FormLayout = () => {
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
-
-const goToReview = () =>{
-  navigate('/review');  
-}
-
+  const goToReview = () => {
+    navigate("/review");
+  };
 
   return (
     <div>
-     
-      <h1 className='Complete'>
-        Complete <span className='Rest'>Your Evidence of Insurability Form</span>
+      <h1 className="Complete">
+        Complete{" "}
+        <span className="Rest">Your Evidence of Insurability Form</span>
       </h1>
 
- 
+      <div className="parent">
+        <div className="medical-questions-title"> EOI Form </div>
 
-      <div className='parent'>
-        <div className='medical-questions-title'> EOI Form </div>
-        
-        <div className='jsonForms'>
+        <div className="jsonForms">
           {/* <Stepper/> */}
-          <div className='section'>
-          <PersonalInformation/>
-          <LifeStyleSection index={0} />
-          <MedicalSection/>
+          <div className="section">
+            <PersonalInformation />
+            <LifeStyleSection index={0} />
+            <MedicalSection />
+          </div>
         </div>
-        </div>
-        <button className='next-button' onClick={goToReview}>Next Page</button>
-        <Sidebar/>
-        </div>
+        <button className="next-button" onClick={goToReview}>
+          Next Page
+        </button>
+        <Sidebar />
+      </div>
     </div>
   );
-}
+};
 
 export default FormLayout;

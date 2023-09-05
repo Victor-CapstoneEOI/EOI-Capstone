c90b6b37eba97dbf5bf00eca34ac686a7c5ec3df
Your Name <you@example.com>
2023-09-04 23:27:51 -0400

    FrenchProfileUpdate

--- src/Pages/FormLayout.jsx
+++ src/Pages/FormLayout.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import {useContext} from 'react';
 import { useNavigate } from 'react-router';
-import { PersonalInformation } from '../sections/personalInformation.jsx';
+import { PersonalInformation } from '../sections/PersonalInformation.jsx';
+import { PastApplications } from '../sections/PastApplications.jsx';
 import { LifeStyleSection } from '../sections/LifeStyleSection.jsx';
+import { Wellness } from '../sections/Wellness.jsx';
 import { MedicalSection } from '../sections/MedicalSection.jsx';
 import Sidebar from '../Components/Sidebar.jsx';
 import '../Styles/FormLayout.css';
 import StepperBar from '../Components/Stepper.jsx';
-
+import Review from './Review.jsx';
+import FormContext from '../Components/FormContext.jsx';
 
 export const FormLayout = () => {
+  const {activeSection} = useContext(FormContext)
   const navigate = useNavigate();
-  
+
+  const steps = ['Personal Information', 'Past Applications', 'Lifestyle', 'Wellness', 'Medical', 'Review']
+
+  let title = steps[activeSection] 
 
   const goToReview = () => {
     navigate("/review");
@@ -23,21 +30,21 @@ export const FormLayout = () => {
       </h1>
 
       <div className="parent">
-       
-        <div className="medical-questions-title">EOI Form </div>
-        
+        <div className="medical-questions-title"> {title} </div>
         
         <div className="stepper-container"> 
         
-          <StepperBar steps={['Personal Information', 'Past Applications', 'Lifestyle', 'Wellness', 'Medical', 'Review']} />
+          <StepperBar steps={steps} />
         </div>
      
         <div className="jsonForms">
 
           <div className="section">
-            <PersonalInformation />
-            <LifeStyleSection index={0} />
-            <MedicalSection />
+            {activeSection == 0 &&<PersonalInformation />}
+            {activeSection == 1 &&<PastApplications />}
+            {activeSection == 2 && <LifeStyleSection index={0} />}
+            {activeSection == 3 && <Wellness />}
+            {activeSection == 4 && <MedicalSection />}
           </div>
         </div>
         <button className="next-button" onClick={goToReview}>
